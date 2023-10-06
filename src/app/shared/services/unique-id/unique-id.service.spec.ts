1e07adef920a2393ed4daefe2bedddf9e74b2ddb
juanprado <juan.prado@cacaushow.com.br>
2023-10-06 19:38:05 -0300

    feat: adição do beforeEach

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -2,10 +2,29 @@ import { UniqueIdService } from './unique-id.service';
 
 describe(UniqueIdService.name, () => {
   // Forma de deixar o nome do método sempre atualizado exemplo, importando a classe do serviço e passando o nome
+
+  let service: UniqueIdService = null;
+  // beforeEach executa algo sempre antes de cada it(), como no exemplo, ele instancia um novo serviço de uniqueid para cada teste
+  beforeEach(() => {
+    service = new UniqueIdService();
+  });
   it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should generate id when called with prefix`, () => {
     // Forma de deixar o nome do método sempre atualizado exemplo, importando a classe do serviço e passando o nome do que esta sendo executado
-    const service = new UniqueIdService();
     const id = service.generateUniqueIdWithPrefix('app');
-    expect(id).toContain('app-');
+    expect(id.startsWith('app-')).toBeTrue();
+  });
+
+  it(`#${UniqueIdService.prototype.generateUniqueIdWithPrefix.name} should not generate duplicate IDs when called multiple times`, () => {
+    const ids = new Set();
+    for (let i = 0; i < 50; i++) {
+      ids.add(service.generateUniqueIdWithPrefix('app'));
+    }
+    expect(ids.size).toBe(50);
+  });
+
+  it(`${UniqueIdService.prototype.getNumberOfGeneratedUniqueIds.name} should return the number of generatedIds when called`, () => {
+    service.generateUniqueIdWithPrefix('app');
+    service.generateUniqueIdWithPrefix('app');
+    expect(service.getNumberOfGeneratedUniqueIds()).toBe(2);
   });
 });
